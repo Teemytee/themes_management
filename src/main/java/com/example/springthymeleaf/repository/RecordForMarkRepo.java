f929713c235f92d6cb04be0c15fdb37b2ce36f1b
Teemytee <lesterfenix@gmail.com>
2023-05-30 22:14:00 +0300

    Delete some classes

--- src/main/java/com/example/springthymeleaf/repository/RecordForMarkRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.springthymeleaf.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.example.springthymeleaf.entity.RecordForMarkEntity;
-
-public interface RecordForMarkRepo extends CrudRepository<RecordForMarkEntity, Long> {
-
-}
